4d273246280db67c883db1dfe067052a64fb17b7
Timo Paulssen <timonator@perpetuum-immobile.de>
2012-10-02 02:09:57 +0200

    construct more semicoly

--- ZasimCodeGenerator.h
+++ ZasimCodeGenerator.h
@@ -368,6 +368,8 @@ private:
 					default:
 						error << "Error: not expected this kind of cell on the right side " << pic->get(i,j)->getType() << " should have been caught by semantics analyser" << endl;
 					}
+					if (!python_mode)
+						toOutStream << ";";
 					toOutStream << endl;
 				} else if (program->head.getCell()->get(i,j)->getType() != EMPTY){
 					toOutStream << "        result_" << attr(i,j) << " = ";
